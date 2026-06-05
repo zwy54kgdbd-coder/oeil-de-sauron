3e7006e3ffca0103356e23fd28e8686ca3522d1c
b3reta <mret@Host-004.lan>
2026-06-05 14:42:18 +0200

    fix missing comma

--- api/delete-user.js
+++ api/delete-user.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseAdmin = createClient(
-  process.env.SUPABASE_URL,
+  process.env.VITE_SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
